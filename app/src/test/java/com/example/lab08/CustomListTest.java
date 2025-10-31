27414e61ff71aebb9a0acb3a88fbb88906109689
Linh <64632034+cheese0987@users.noreply.github.com>
2025-10-31 13:23:58 -0600

    Write a Failing Test for countCities()

--- app/src/test/java/com/example/lab08/CustomListTest.java
+++ app/src/test/java/com/example/lab08/CustomListTest.java
@@ -1,4 +1,5 @@
 package com.example.lab08;
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -25,4 +26,17 @@ public class CustomListTest {
         // Assertion
         assertFalse(list.hasCity(calgary));
     }
+
+    @Test
+    public void testCountCities() {
+        // Setup
+        CustomList list = new CustomList();
+        list.addCity(new City("Calgary", "AB"));
+        list.addCity(new City("Edmonton", "AB"));
+
+        // Action & Assertion
+        // We expect the count to be 2.
+        Assert.assertEquals(2, list.countCities()); // This method doesn't exist yet!
+    }
+
 }
